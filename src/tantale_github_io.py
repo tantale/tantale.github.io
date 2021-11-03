20d4ca9b548923d7dfbb71d4de69fb76cf759786
Laurent LAPORTE <tantale.solutions@gmail.com>
2021-11-03 21:28:25 +0100

    Update the project configuration to build a static website with mkdocs.

---
+++ src/tantale_github_io.py
@@ -0,0 +1,9 @@
+"""
+Tantale’s Blog Generator
+
+Empty Python project used to generate https://tantale.github.io/ Github Pages.
+"""
+
+__version__ = "0.1.0"
+__date__ = "unreleased"
+__credits__ = "© 2021-2022 Tantale Solutions"
